420b067691671b3c1652485b2e0af23e78412c22
Johannes Preisinger <cnnwarthem@gmail.com>
2020-10-14 16:56:58 +0200

    Backend connected with Frontend. Plotting backend data

--- src/App.vue
+++ src/App.vue
@@ -3,87 +3,94 @@
     <form action="#" @submit.prevent="getIssues">
       <div class="form-group">
         <input
-                type="text"
-                placeholder="owner/repo Name"
-                v-model="repository"
-                class="col-md-2 col-md-offset-5"
+            type="text"
+            placeholder="owner/repo Name"
+            v-model="repository"
+            class="col-md-2 col-md-offset-5"
         >
       </div>
+      <div id="my_dataviz"></div>
       <div class="alert alert-info" v-show="loading">Loading...</div>
       <div class="alert alert-danger" v-show="errored">An error occured</div>
-      <chart :issues="issues"></chart>
     </form>
   </div>
 </template>
 
 <script>
-  import moment from "moment";
-  import axios from "axios";
-  import Chart from './components/charts/Chart.vue';
+import * as d3 from "d3";
 
-  export default {
-    name: "app",
-    components: {
-      Chart
-    },
-    data() {
-      return {
-        loading: false,
-        errored: false,
-        issues: [],
-        repository: "",
-        startDate: null
-      };
-    },
-    methods: {
-      getDateRange() {
-        const startDate = moment().subtract(6, 'days');
-        const endDate = moment();
-        const dates = [];
+export default {
+  name: "app",
+  components: {},
+  data() {
+    return {
+      loading: false,
+      errored: false,
+      repository: "",
+      startDate: null,
+      chart: null
+    };
+  },
+  methods: {
+    getIssues() {
+      this.loading = true;
+      this.errored = false;
 
-        while (startDate.isSameOrBefore(endDate)) {
-          dates.push({
-            day: startDate.format('MMM Do YY'),
-            issues: 0
-          });
+      d3.select("#my_dataviz").selectAll("svg").remove()
+      //2_TwoNum.csv
+      d3.json("http://localhost:5000/api/v1/clusters/data/files/" + this.repository).then(function (data) {
+        var margin = {top: 10, right: 30, bottom: 30, left: 60},
+            width = 1000 - margin.left - margin.right,
+            height = 700 - margin.top - margin.bottom;
 
-          startDate.add(1, 'days');
-        }
+// append the svg object to the body of the page
+        var svg = d3.select("#my_dataviz")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
 
-        return dates;
-      },
-      getIssues() {
-        this.loading = true;
-        this.errored = false;
-        this.startDate = moment()
-                .subtract(6, "days")
-                .format("YYYY-MM-DD");
+        // Add X axis
+        var x = d3.scaleLinear()
+            .domain([0, 1])
+            .range([0, width]);
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
 
-        axios
-                .get(
-                        `https://api.github.com/search/issues?q=repo:${this.repository}+is:issue+is:open+created:>=${this.startDate}`,
-                        { params: { per_page: 100 } }
-                )
-                .then(response => {
-                  const payload = this.getDateRange();
+        // Add Y axis
+        var y = d3.scaleLinear()
+            .domain([0, 1])
+            .range([height, 0]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
 
-                  response.data.items.forEach(item => {
-                    const key = moment(item.created_at).format("MMM Do YY");
-                    const obj = payload.filter(o => o.day === key)[0];
-                    obj.issues += 1;
-                  });
-
-                  this.issues = payload;
-                  console.log(this.issues);
-                })
-                .catch(error => {
-                  this.errored = true;
-                  console.error(error);
-                })
-                .finally(() => (this.loading = false));
-      }
+        // Add dots
+        svg.append('g')
+            .selectAll("dot")
+            .data(data.data)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) {
+              return x(d[0]);
+            })
+            .attr("cy", function (d) {
+              return y(d[1]);
+            })
+            .attr("r", 1.5)
+            .style("fill", "#ff0000")
+      })
+          .catch(error => {
+            this.errored = true;
+            this.chart = null;
+            console.error(error);
+          })
+          .finally(() => (this.loading = false));
     }
-  };
+  }
+};
 </script>
 
 <style>
